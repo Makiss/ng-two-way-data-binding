5611dc4fc024abfd7e9ed28519a0be0fc56e7e9e
syomushkin_m <syomushkin@river-soft.net>
2018-07-09 18:54:10 +0300

    Add scope variables, click handler and watcher

--- app/menuController.js
+++ app/menuController.js
@@ -1,5 +1,16 @@
 app.controller('menuController', ['$scope', function($scope) {
   $scope.model = {
-    title: 'Our Menu'
+    title: 'Our Menu',
+    mainDish: 'No Item Selected'
   };
+
+  $scope.changeMainDish = function(item) {
+    $scope.model.mainDish = item;
+  };
+
+  $scope.$watch('model.mainDish', function(newValue, oldValue) {
+    if (newValue === 'BBQ Chicken Pizza') {
+      alert('You have selected the BBQ Chicken Pizza!');
+    }
+  });
 }]);
